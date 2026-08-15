18484fc3e0bba0cfa6b9b40f7b82a54891a9b5e5
Divyam <divyambkp06@gmail.com>
2026-08-15 21:53:49 +0530

    chore: fix .gitignore and remove build artifacts from tracking

--- app/build/generated/hilt/component_trees/debug/dagger/hilt/internal/processedrootsentinel/codegen/_com_timebasedfitness_app_TimeBasedFitnessApp.java
+++
@@ -1,9 +0,0 @@
-package dagger.hilt.internal.processedrootsentinel.codegen;
-
-import dagger.hilt.internal.processedrootsentinel.ProcessedRootSentinel;
-
-@ProcessedRootSentinel(
-    roots = "com.timebasedfitness.app.TimeBasedFitnessApp"
-)
-public final class _com_timebasedfitness_app_TimeBasedFitnessApp {
-}
